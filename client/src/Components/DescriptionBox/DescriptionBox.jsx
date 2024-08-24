636414e15141a8c44ca79d94771c4f909b1eb244
pranathi198lq <97428301+pranathi198lq@users.noreply.github.com>
2024-08-24 22:36:03 +0530

    Create DescriptionBox.jsx

---
+++ client/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Descriptionbox.css'
+
+export const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="navigator">
+                <div className="navbox">Description</div>
+                <div className="navbox fade">Reviews(122)</div>
+            </div>
+            <div className="desc">
+                <p>It is an official online platform catering to the latest fashion trends and accomodates the styles for various topological people, different sizes and preferences. It showcases the products, interacts with the customers with the cart facilities and convinient browsing</p>
+                <p>Stellar displays products along with the descriptions and caters to the interests of all the people when it comes to clothing and helps keep up with the latest fashion trends that can be of preference to the customer.</p>
+    
+            </div>
+        </div>
+      )
+}
